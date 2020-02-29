cf4f4e9f48115bf1a4d6e63a44c6066cf145ffcc
Alice <alicenjoroge707@gmail.com>
2020-02-29 13:46:36 +0300

    display a user with todos object

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,7 +9,7 @@ const errHandler = (err, res) => {
 
 const index = async (req, res) => {
     try {
-        const users = await db.users.findAll({attributes: ["name", "email", "createdAt"]});
+        const users = await db.users.findAll({attributes: ["name", "email", "createdAt"], include:"tasks"});
         return res.json(users);
     } catch (e) {
         return errHandler(e, res);
